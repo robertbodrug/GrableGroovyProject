01ab1a2201822ffcfa931cea4472b3563709003c
robertbodrug <robertobodrug@gmail.com>
2023-11-11 17:30:05 +0100

    final

---
+++ src/main/java/com/example/Human.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Human {
+    private String name;
+
+
+    private String lastName;
+
+    public Human(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
+
